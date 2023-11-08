f367d2770ea0a5b029847d66bc82885f80aa4fee
Syad K <syadkhwajazada@gmail.com>
2023-11-08 17:28:05 -0500

    update resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,7 +17,7 @@ const greeting = {
   title: "Hey, I'm Syad Khwajazada",
   logo_name: "Syad Khwajazada",
   subTitle: "I am a recent graduate for Computer Engineering at the University of Guelph with a focus on Software.  I am really passionate about Mobile/Web Development, Machine Learning, Basketball, Music and Video Games.",
-  resumeLink: "https://drive.google.com/file/d/10EHH8Jfuy7CQomU-x4b6L-GCOKPh2VnO/view?usp=sharing",
+  resumeLink: "https://drive.google.com/file/d/1vaUxnMDuOTwXJ1I37_9Yq9wne_XMfSYM/view?usp=sharing",
   portfolio_repository: "https://github.com/syadk23/syadk23.github.io",
   githubProfile: "https://github.com/syadk23",
 };
